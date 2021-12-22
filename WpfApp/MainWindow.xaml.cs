9d94f16a44f7e30cb11774204a92b79fb43de474
ZKXGO <74192648+ZKXGO@users.noreply.github.com>
2021-12-22 12:11:29 +0300

    Add files via upload

---
+++ WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ModelLibrary;
+using Ookii.Dialogs.Wpf;
+using Microsoft.EntityFrameworkCore;
+//using System.Linq;
+using System.Drawing;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+    public partial class MainWindow : Window
+    {
+        public static ResultContext db = new ResultContext();
+        public static ObservableCollection<Results> resultCollection = db.Results.Local.ToObservableCollection();
+
+        private static async Task Consumer()
+        {
+            while (true)
+            {
+                string type;
+                Bitmap bitmap;
+                float[] BBox;
+
+                (type, bitmap, BBox) = await Detector.resultBufferBlock.ReceiveAsync();
+                if (type == "end")
+                {
+                    db.SaveChanges();
+                    break;
+                }
+                else
+                {
+                    db.AddElem(type, BBox, bitmap);
+                }
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = resultCollection;
+        }
+
+        private void btnflod_C(object sender, RoutedEventArgs e)
+        {
+            VistaFolderBrowserDialog dialog = new VistaFolderBrowserDialog();
+            if ((bool)dialog.ShowDialog())
+                TextBox_Path.Text = dialog.SelectedPath;
+        }
+
+        private async void btnRun_C(object sender, RoutedEventArgs e)
+        {
+            btnRun.IsEnabled = false;
+            db.Clear();
+            Detector.cancelTokenSource = new CancellationTokenSource();
+            Detector.token = Detector.cancelTokenSource.Token;
+
+            await Task.WhenAll(Detector.DetectImage(TextBox_Path.Text), Consumer());
+            btnRun.IsEnabled = true;
+        }
+
+        private void btnCancel_C(object sender, RoutedEventArgs e)
+        {
+            Detector.cancelTokenSource.Cancel();
+            btnRun.IsEnabled = true;
+        }
+
+        private void btnDelete_C(object sender, RoutedEventArgs e)
+        {
+            db.DeleteType(listBox_types.SelectedItem.ToString());
+        }
+    }
+}
---
+++ WpfApp/ResultContext.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.IO;
+using System.Drawing;
+using System.Collections;
+
+namespace WpfApp
+{
+    public class DetectedObject
+    {
+        public int DetectedObjectId { get; set; }
+        
+        public float x1 { get; set; }
+        public float y1 { get; set; }
+        public float x2 { get; set; }
+        public float y2 { get; set; }
+        public byte[] BitmapImage { get; set; }
+        
+        public Results Type { get; set; }
+       
+    }
+
+    public class Results
+    {
+        public int ResultsId { get; set; }
+        public string Type { get; set; }
+        public ICollection<DetectedObject> DetectedObjects { get; set; }
+
+        public override string ToString()
+        {
+            return Type;
+        }
+    }
+    public class ResultContext : DbContext
+    {
+        public DbSet<Results> Results { get; set; }
+        public DbSet<DetectedObject> DetectedObject { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder o)
+           => o.UseSqlite(@"Data Source=");
+
+        public static byte[] ImageToByte2(Image img)
+        {
+            using (var stream = new MemoryStream())
+            {
+                img.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                return stream.ToArray();
+            }
+        }
+
+        public void AddElem (string type, float[] BBox, Bitmap bitmap)
+        {
+            var dobj = new DetectedObject();
+            byte[] byteArrImage = ImageToByte2(bitmap);
+
+            dobj.Type = new Results();
+            var query = Results.Where(p => type == p.Type);
+            if (query.Count() > 0)
+            {
+                dobj.Type = query.First();
+            } 
+            else
+            {
+                dobj.Type = new Results();
+                dobj.Type.Type = type;
+                Results.Add(dobj.Type);
+            }
+
+            var f = DetectedObject.Where(p => p.x1 == BBox[0] && p.y1 == BBox[1] && p.x2 == BBox[2] && p.y2 == BBox[3]
+                                         && p.Type.Type == type);
+
+            if (f.Count() == 0 || !byteArrImage.SequenceEqual(f.First().BitmapImage))
+            {
+                dobj.x1 = BBox[0];
+                dobj.y1 = BBox[1];
+                dobj.x2 = BBox[2];
+                dobj.y2 = BBox[3];
+                dobj.BitmapImage = byteArrImage;
+                DetectedObject.Add(dobj);
+            }
+            
+            SaveChanges();
+        }
+
+        public IEnumerable<string> GetTypes()
+        {
+            foreach (var res in Results)
+            {
+                yield return res.Type;
+            }
+        }
+
+        
+        public void Clear()
+        {
+            foreach (var dobj in DetectedObject)
+            {
+                DetectedObject.Remove(dobj);
+            }
+            foreach (var res in Results)
+            {
+                Results.Remove(res);
+            }
+            SaveChanges();
+        }
+        
+
+        public void DeleteType(string type)
+        {
+            foreach (var dobj in DetectedObject)
+            {
+                if (dobj.Type.Type == type)
+                {
+                    DetectedObject.Remove(dobj);
+                }
+            }
+            foreach (var res in Results)
+            {
+                if (res.Type == type)
+                {
+                    Results.Remove(res);
+                }
+            }
+
+            SaveChanges();
+        } 
+    }
+}
